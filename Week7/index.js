94dbdc4bc0af5fea2d0060b427f3d38db5f4761a
Anubrata Guin <141846655+AnuGuin@users.noreply.github.com>
2025-09-07 18:18:33 +0530

    Update index.js

--- Week7/index.js
+++ Week7/index.js
@@ -11,7 +11,7 @@ const JWT_SECRET = "mysupersecretkey"
 
 async function connectDB() {
     try {
-        await mongoose.connect("mongodb+srv://Anu:Ag%401508@cluster0.fmem1n5.mongodb.net/TodoApp");
+        await mongoose.connect("URI");
         console.log("Connected to MongoDB");
     } catch (e) {
         console.error("MongoDB connection error:", e.message);
